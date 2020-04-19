d8492e56c98dc05091ceb603795f756f6a17423d
maly7 <paul.aly@outlook.com>
2020-04-19 09:08:19 -0400

    Initial Project creation from start.spring.io

---
+++ src/main/java/com/lumberjackdev/springbooktk8s/Springbooktk8sApplication.java
@@ -0,0 +1,13 @@
+package com.lumberjackdev.springbooktk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooktk8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbooktk8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumberjackdev/springbooktk8s/Springbooktk8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumberjackdev.springbooktk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbooktk8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
